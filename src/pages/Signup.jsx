b1d216cf91ebd9253d3a0799510e62b575c876cb
leesoojin <leesoojin800@gmail.com>
2023-08-08 15:22:26 +0900

    design : 회원가입 ui

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import * as S from "../styles/style.signup";
 
 function Signup() {
-  return <div>Signup</div>;
+  return (
+    <div>
+      <S.Container>
+        <S.SignupBoxLocation>
+          <form>
+            <S.Space>
+              <S.Email placeholder="이메일" name="email" />
+            </S.Space>
+            <S.Space>
+              <S.Nickname placeholder="닉네임" name="nickname" />
+            </S.Space>
+            <S.Space>
+              <S.Password
+                placeholder="비밀번호"
+                name="password"
+                type="password"
+              />
+            </S.Space>
+            <S.Space>
+              <S.PassWordConfirm
+                placeholder="비밀번호 확인"
+                name="passwordConfirm"
+                type="password"
+              />
+            </S.Space>
+            <S.Space>
+              <S.LoginButton type="submit">회원가입하기</S.LoginButton>
+            </S.Space>
+            <S.SignupButton>로그인하러 가기</S.SignupButton>
+          </form>
+        </S.SignupBoxLocation>
+      </S.Container>
+    </div>
+  );
 }
 
 export default Signup;
